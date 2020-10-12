888794881861416248544fabca21f1be323c6028
flawn <flawnsontong1@gmail.com>
2020-10-12 18:45:19 -0400

    Added (And commented out) code for GitHub calendar

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -3,3 +3,7 @@ window.addEventListener("load",function lastMod() {
     var lastMod = "Last Edited: " + document.lastModified;
     document.getElementById("modTime").innerHTML = lastMod;
 },false);
+
+// window.addEventListener("load", function commit() {
+//     GitHubCalendar(".calendar", "flawnson", {responsive: true});
+// }, false);
