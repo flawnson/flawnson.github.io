874e80a1253af17ad3a5fa39498f1a5590f358d0
flawnson <flawnsontong1@gmail.com>
2020-11-02 22:23:22 -0500

    Turned Github cal into local sourcing

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,7 +1,9 @@
 // To add last edited time to footer of website
 window.addEventListener("load",function lastMod() {
-    var lastMod = "Last Edited: " + document.lastModified;
-    document.getElementById("modTime").innerHTML = lastMod;
+    let lastMod = "Last Edited: " + document.lastModified;
+    if (document.getElementById("modTime")) {
+        document.getElementById("modTime").innerHTML = lastMod;
+    }
 },false);
 
 window.addEventListener("load", function commit() {
